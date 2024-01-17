7401aaa100d8b3018c83c501995965caec88436d
saivaraprasad195 <62549091+saivaraprasad195@users.noreply.github.com>
2024-01-18 00:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let arr = Object.keys(sampleObject);
+	if(arr.includes(key)) return true;
+	return false;
 }
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
